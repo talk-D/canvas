cbe0ada47851bcf2e7d873e880ccbe241fdd5316
okxooxoo <okxooxoo@naver.com>
2024-05-18 15:22:14 +0900

    ♻️ refactor: 필요 없는 코드 삭제 및 정리

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import '../styles/Theme.css';
 import { ChromePicker } from 'react-color';
-import domtoimage from 'dom-to-image';
-import { saveAs } from 'file-saver';
 import { FigureIcon, BasicIcon, StorageIcon, TextIcon } from '../icons/MenuIcon';
 
 const Theme = () => {
     const [frameColor, setFrameColor] = useState('#ffffff');
+    // 색상 선택기 표시 여부
     const [showColorPicker, setShowColorPicker] = useState(false);
+    // 색상 선택기 박스(상단바) 표시 여부
     const [showColorPickerBox, setShowColorPickerBox] = useState(false);
-    const [isDragging, setIsDragging] = useState(false);
 
+    // 여러 사각형의 id, 색상, 위치, 크기 정보를 가지는 배열
     const [rectangles, setRectangles] = useState([]);
+    // 사용자가 선택한 도형의 id
     const [selectedFigureId, setSelectedFigureId] = useState(-1);
 
+    // 프레임의 참조
     const frameRef = useRef(null);
+    // 색상 선택기의 참조
     const ChromePickerRef = useRef(null);
 
-    const downloadFrame = () => {
-        domtoimage
-            .toSvg(document.querySelector('.frame'))
-            .then((blob) => {
-            saveAs(blob, 'mytheme.svg');
-        });
-    };
-
+    // 도형 아이콘을 클릭하면 실행하는 함수
     const handleRectangleIconClick = () => {
         const newRectangle = {
             id: rectangles.length,
@@ -38,11 +34,7 @@ const Theme = () => {
         setRectangles([...rectangles, newRectangle]);
     };
 
-    const handleRectangleMouseDown = (id) => {
-        setSelectedFigureId(id);
-        setIsDragging(true);
-    };
-
+    // 영역 밖을 클릭하면 실행하는 함수
     const handleContainerClick = (e) => {
         if (e.target.classList.contains('container')) {
             frameRef.current.style.border = 'none';
@@ -52,6 +44,7 @@ const Theme = () => {
         }
     };
 
+    // 영역 안(프레임)을 클릭하면 실행하는 함수
     const handleFrameClick = (e) => {
         if (e.target.classList.contains('frame')) {
             frameRef.current.style.border = '3px solid #FF9900';
@@ -60,61 +53,23 @@ const Theme = () => {
         }
     };
 
+    // 색상을 변경하는 함수
     const handleColorChange = (color) => {
         setFrameColor(color.hex);
     };
 
+    // 색상 변경 아이콘을 클릭하면 실행하는 함수
     const handleColorPickerIconClick = () => {
         setShowColorPicker(!showColorPicker);
     };
 
-    const moveFigure = (e) => {
-        if (!isDragging || selectedFigureId === null) return;
-        
-        const newRectangles = rectangles.map((rectangle) => {
-            if (rectangle.id === selectedFigureId) {
-                console.log('x', e.clientX);
-                console.log('y', e.clientY);
-                console.log(rectangle);
-                return {
-                  ...rectangle,
-                  left: e.clientX - rectangle.width / 2,
-                  top: e.clientY - rectangle.height / 2,
-                };
-            } else {
-                return rectangle;
-            }
-        });
-
-        setRectangles(newRectangles);
-    }
-
-    const handleMouseMove = (e) => {
-      if (!isDragging) return;
-      moveFigure(e);
-    };
-  
-    const handleMouseUp = () => {
-      setIsDragging(false);
-    };
-
-    useEffect(() => {
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mouseup', handleMouseUp);
-      
-        return () => {
-          window.removeEventListener('mousemove', handleMouseMove);
-          window.removeEventListener('mouseup', handleMouseUp);
-        };
-
-    }, [isDragging, selectedFigureId, rectangles, handleMouseMove]);
-      
-
     return (
         <div className='container' onClick={handleContainerClick}>
+            {/* showColorPickerBox가 true이면 ColorPickerBox(상단바)를 보여줍니다. */}
             {showColorPickerBox &&
             <div className='color-picker-box'>
                 <img onClick={handleColorPickerIconClick} alt='색상 선택' src='color-picker.png' className="color-picker-icon" />
+                {/* showColorPicker가 true이면 ColorPicker(색상선택기)를 보여줍니다. */}
                 {showColorPicker &&
                 <ChromePicker
                     ref={ChromePickerRef}
@@ -122,6 +77,7 @@ const Theme = () => {
                     onChange={handleColorChange}
                 />}
             </div>}
+            {/* 왼쪽 메뉴바 */}
             <div className='left-menubar'>
                 <div className='menu-icon'>
                     <FigureIcon />
@@ -136,6 +92,7 @@ const Theme = () => {
                     <StorageIcon />
                 </div>
             </div>
+            {/* 도형 아이콘 클릭 시 노출되는 Drawer, 추후 열고 닫는 기능 구현 필요 */}
             <div className='left-drawer'>
                 <div onClick={handleRectangleIconClick} className='drawer-icon'>
                     Rectangle
@@ -150,7 +107,9 @@ const Theme = () => {
                     Star
                 </div>
             </div>
+
             <div ref={frameRef} onClick={handleFrameClick} className='frame' style={{ backgroundColor: frameColor }}>
+            {/* 사용자가 생성한 사각형을 모두 화면에 생성 */}
             {rectangles.map((rectangle) => (
                 <div
                     className='rectangle'
@@ -165,7 +124,6 @@ const Theme = () => {
                         backgroundColor: rectangle.color,
                         border: selectedFigureId === rectangle.id ? '3px solid #FF9900' : 'none',
                     }}
-                    onMouseDown={() => handleRectangleMouseDown(rectangle.id)}
                 />
             ))}
             </div>
