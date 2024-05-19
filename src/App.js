493265cd2a4a340cef472fdc2fc972cfbb198343
hyein <ttmmad@naver.com>
2024-05-19 12:05:39 +0900

    feat: 폰트변경(혜인)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
 import Theme from './pages/Theme';
+import Step1 from './pages/Step1';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
            <Route path='/' element={<Main />} />
+           <Route path='/basic/step1' element={<Step1 />} />
           <Route path='/theme' element={<Theme />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Step1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Logo} from '../header/Logo.jsx';
+import '../styles/Step1.css';
+
+
+function Step1() {
+    
+
+    return (
+        <div className='div-wrapper'>
+            <Logo />
+            <div className='menu'>
+                <div className='first-row'>
+                    <div className='step1'>STEP 1. 테마 정보</div>
+                    <div className='step2'>STEP 2. 테마 제작</div>
+                    <div className='step3'>STEP 3. 테마 다운</div>
+
+                </div>
+                <div className='second-row'>
+                <div className='thumbnail'>썸네일</div>
+                <div className='password'>비밀번호</div>
+                <div className='friendlist'>친구 목록</div>
+                <div className='tabbar'>탭바</div>
+                <div className='chatroom'>채팅방</div>
+                <div className='notification'>알림창</div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Step1;
