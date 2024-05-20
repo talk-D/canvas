166b66afbb7ba2ffa4ae128781983e93a5dc2922
hyein <ttmmad@naver.com>
2024-05-20 21:08:11 +0900

    feat: step1(혜인)

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Themeguide } from '../header/Themeguide.jsx';
-import Theme from './Theme.jsx';
+import {Megaphone} from "../icons/MainPageIcon";
+import '../styles/Step1.css';
 
 
 function Step1() {
-    
 
     return (
         <div className='wrap'>
             <Themeguide />
-            <Theme />
+          <div className = 'background'>
+              <div className='guide'> <Megaphone /> 테마정보를 입력해주세요!</div>
+              <div className='step1-input'>
+                  <div className='theme-version'>
+                    테마버전
+                    <div className='text-input'> <input type="text"></input></div>
+                  </div>
+                  <div className='theme-name-Korean'>
+                    테마 이름(한글)
+                    <div className='text-input'> <input type="text"></input></div>
+                  </div>
+                      <div className='theme-name-English'>
+                    테마 이름(영문)
+                    <div className='text-input'> <input type="text"></input></div>
+                  </div>
+              </div>
+          </div>
         </div>
     );
 }
