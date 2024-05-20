e3628bef4b83e7ef24c1b928462ccf2c87a4fc9b
hyein <ttmmad@naver.com>
2024-05-21 00:21:51 +0900

    feat: step1(혜인)

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -11,18 +11,18 @@ function Step1() {
             <Themeguide />
           <div className = 'background'>
               <div className='info-wrap'>
-              <div className='guide'> <Megaphone /> &nbsp;&nbsp;테마정보를 입력해주세요!</div>
+              <div className='guide'> <Megaphone /> &nbsp;&nbsp;테마 정보를 입력해주세요!</div>
               <div className='step1-input'>
                   <div className='theme-version'>
-                    테마버전
+                    테마 버전&nbsp;&nbsp;&nbsp;
                     <div className='text-input'> <input type="text"></input></div>
                   </div>
                   <div className='theme-name-Korean'>
-                    테마 이름(한글)
+                    테마 이름(한글)&nbsp;&nbsp;&nbsp;
                     <div className='text-input'> <input type="text"></input></div>
                   </div>
                       <div className='theme-name-English'>
-                    테마 이름(영문)
+                    테마 이름(영문)&nbsp;&nbsp;&nbsp;
                     <div className='text-input'> <input type="text"></input></div>
                   </div>
               </div>
