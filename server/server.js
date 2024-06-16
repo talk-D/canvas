a1fbcf1b6ec98c30e59e939ef0f4ddef2b7fb83e
hyein <ttmmad@naver.com>
2024-06-16 19:20:05 +0900

    ec2 배포 오류 무시

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = 5000;
@@ -26,6 +27,14 @@ app.use('/friendlist', friendlistRoute);
 app.use('/chatroom', chatroomRoute);
 app.use('/imageProcessing', imageProcessingRoute);
 app.use('/saveThumbnail', saveThumbnailRoute); // 새로운 라우트 추가
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'build')));
+
+// 모든 요청을 React 애플리케이션으로 라우팅
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
